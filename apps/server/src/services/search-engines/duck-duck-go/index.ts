5b6c310ce8d81377de4733768f786c9887ea5886
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-14 11:49:30 -0300

    feat: add DuckDuckGo search engine

---
+++ apps/server/src/services/search-engines/duck-duck-go/index.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { SearchEngine } from '~/services/search-engines/contract';
+import { DuckDuckGoSearchResponse } from '~/services/search-engines/duck-duck-go/types';
+import Env from '~/shared/env';
+
+@Injectable()
+export class DuckDuckGo implements SearchEngine<DuckDuckGoSearchResponse> {
+  private readonly apiUrl: string;
+
+  constructor() {
+    this.apiUrl = Env.duckDuckGoApiUrl;
+  }
+
+  private async fetch(searchTerm: string): Promise<DuckDuckGoSearchResponse> {
+    const response = await fetch(`${this.apiUrl}?q=${searchTerm}?format=json`);
+    return response.json();
+  }
+
+  async search(query: string): Promise<DuckDuckGoSearchResponse> {
+    return this.fetch(query);
+  }
+}
