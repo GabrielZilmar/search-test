a92f6dacceba005fa717ec531c2df230733ac0cd
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-14 16:35:39 -0300

    feat: add QueryProviders

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@cialdnb/ui/globals.css";
+import QueryProviders from "~/lib/query-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <QueryProviders>{children}</QueryProviders>
+      </body>
     </html>
   );
 }
