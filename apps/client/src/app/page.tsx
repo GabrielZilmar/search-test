927fd389deae575c1d7776378043d57cef64316d
Gabriel Zilmar <51098008+GabrielZilmar@users.noreply.github.com>
2024-08-14 14:05:25 -0300

    Feature/setup client (#4)
    * feat: init client
    
    * feat: ui package
    
    * feat: update global.css styles

---
+++ apps/client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@cialdnb/ui";
+
+export default function Home() {
+  return (
+    <main>
+      <Button>Click me</Button>
+    </main>
+  );
+}
---
+++ apps/client/tailwind.config.ts
@@ -0,0 +1,1 @@
+export * from "@cialdnb/ui/tailwind.config";
