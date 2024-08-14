e510412647a954023e8b9d61049ca2a9b3d73d8f
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-14 12:29:22 -0300

    feat: add search endpoint

--- apps/server/src/modules/searches/search.module.ts
+++ apps/server/src/modules/searches/search.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from '~/modules/searches/search.controller';
+import searchesUseCaseProviders from '~/modules/searches/use-cases/providers';
+import { DuckDuckGo } from '~/services/search-engines/duck-duck-go';
 
 @Module({
   controllers: [SearchController],
-  providers: [],
+  providers: [...searchesUseCaseProviders, DuckDuckGo],
 })
 export class SearchModule {}
