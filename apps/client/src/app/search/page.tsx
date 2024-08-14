89692f916be620e79210451bd6dbc5b50dbc9ef9
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-14 16:48:48 -0300

    feat: add loading and bstract to search page

--- apps/client/src/app/search/page.tsx
+++ apps/client/src/app/search/page.tsx
@@ -6,16 +6,29 @@ import { useSearch } from "~/hooks";
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
-
   const searchTerm = useMemo(() => searchParams.get("q") || "", [searchParams]);
+  const { result, isLoading } = useSearch(searchTerm);
 
-  const { result } = useSearch(searchTerm);
-  console.log("🚀 ~ SearchPage ~ result:", result);
+  if (isLoading) {
+    return (
+      <main>
+        <div className="flex justify-center items-center h-screen">
+          <p>Loading...</p>
+        </div>
+      </main>
+    );
+  }
 
   return (
-    <main>
-      <div className="flex justify-center items-center h-screen">
-        <p>{searchTerm}</p>
+    <main className="h-screen">
+      <div className="p-4 space-y-2">
+        <h1 className="font-bold text-lg">{searchTerm}</h1>
+        <p className="text-sm sm:text-xs">{result?.abstract || ""}</p>
+      </div>
+      <div className="flex justify-center items-center ">
+        {/** DisplayRelatedTopics */}
+        {/** DisplayRelatedTopics */}
+        {/** DisplaySearchResult */}
       </div>
     </main>
   );
