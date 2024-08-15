b0a33ce237d32b31c9328876da73e2f7d7da8b07
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-15 10:50:55 -0300

    feat: init sidebar component

--- apps/client/src/app/home/page.tsx
+++ apps/client/src/app/home/page.tsx
@@ -1,12 +1,11 @@
 import SearchForm from "~/components/forms/search";
 import SearchHistory from "~/components/search-history";
+import Sidebar from "~/components/sidebar";
 
 export default function Home() {
   return (
     <main>
-      <div className="absolute p-4">
-        <SearchHistory />
-      </div>
+      <Sidebar />
       <div className="flex justify-center items-center h-screen">
         <SearchForm />
       </div>
