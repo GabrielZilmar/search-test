9ffd1c962556453ed7efb10d68694d7f606c763b
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-08-14 21:35:44 -0300

    feat: add DisplayRelatedTopics

--- apps/client/src/app/search/page.tsx
+++ apps/client/src/app/search/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { useMemo } from "react";
+import DisplayRelatedTopics from "~/components/display-related-topics";
 import { useSearch } from "~/hooks";
 
 export default function SearchPage() {
@@ -20,13 +21,13 @@ export default function SearchPage() {
   }
 
   return (
-    <main className="h-screen">
-      <div className="p-4 space-y-2">
+    <main className="h-screen p-4 space-y-4">
+      <div className="space-y-2">
         <h1 className="font-bold text-lg">{searchTerm}</h1>
-        <p className="text-sm sm:text-xs">{result?.abstract || ""}</p>
+        <p className="text-sm sm:text-xs">{result.abstract}</p>
       </div>
+      <DisplayRelatedTopics relatedTopics={result.relatedTopics} />
       <div className="flex justify-center items-center ">
-        {/** DisplayRelatedTopics */}
         {/** DisplayRelatedTopics */}
         {/** DisplaySearchResult */}
       </div>
---
+++ apps/client/src/components/display-related-topics/index.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@cialdnb/ui";
+import Link from "next/link";
+import { SearchResultItem } from "~/types/search";
+
+type DisplayRelatedTopicsProps = {
+  relatedTopics: SearchResultItem[];
+};
+
+const DisplayRelatedTopics: React.FC<DisplayRelatedTopicsProps> = ({
+  relatedTopics,
+}) => {
+  return (
+    <div>
+      <h1 className="font-bold text-lg">Related Topics</h1>
+      <div className="flex flex-row space-x-4 overflow-x-auto h-full">
+        {relatedTopics.map((topic) => (
+          <Card key={topic.url} className="flex-1">
+            <Link href={topic.url} target="_blank">
+              <CardHeader>
+                <CardTitle>{topic.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{topic.title}</p>
+              </CardContent>
+            </Link>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DisplayRelatedTopics;
